3ad064e2e5611092a8951fb49bc5bcae658e4961
Basit-Perampalli <apeerampalli@gmail.com>
2023-08-08 01:25:05 +0530

    refactored and handled some unknown errors in auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,20 +8,30 @@ router.post('/', [
     body('name',"Enter a valid name").isLength({ min: 3 }),
     body('email',"Enter a valid email").isEmail({ min: 5 }),
     body('password',"Password must be of min 5 chars").isLength({ min: 5 })
-], (req, res) => {
+], async (req, res) => {
     console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user=>res.json(user))
-    .catch(err=>{console.log(err)
-    res.json({error: "Email already used. Please try with some another email"})})
+    // Check whether the email is used already
 
+    try {
+        let user = await User.findOne({email:req.body.email})
+        if(user){
+            return res.status(400).json({error: "Email already used. Please try with some another email"});
+        }
+        user = await User.create({              // Creating a new user using 
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json(user)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("some error occured")
+    }
+        
 });
 
 module.exports = router;
